e4c531cd9161bf87f1e71ad836e3b806ac3f8587
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-04-10 21:00:14 +0545

    Cross-device response optimized

--- core.js
+++ core.js
@@ -1,4 +1,4 @@
 $(window).on("load", function(){
-	$(".content").css("opacity", "1.0");
-	$(".content").addClass("animated fadeIn")
+	$(".content").css("opacity", "1.0");/*
+	$(".content").addClass("animated fadeIn")*/
 });
