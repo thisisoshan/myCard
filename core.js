11fa998cc9021264c4171611fb2521e4f145212d
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-04-11 10:21:14 +0545

    Javascript Load function optimized

--- core.js
+++ core.js
@@ -1,3 +1,11 @@
 $(window).on("load", function(){
-	$(".content").css("opacity", "1.0");
+	$(".img").addClass("fadeIn").css("opacity", "1.0");
+	$("h1, p, ul").addClass("fadeInRight");
+	$("h1").css("animation-delay", "0.5s");
+	$("p").css("animation-delay", "0.25s");
+	var i = setInterval(function(){setOpaque();}, 1000);
 });
+function setOpaque(){
+	$("h1, p, ul").css("opacity", "1.0")
+	clearInterval(i);
+}
