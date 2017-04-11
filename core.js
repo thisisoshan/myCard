f672e0289fe7c51497662650ee5348b0130a6569
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-04-11 20:25:41 +0545

    favicon and intro optimized

--- core.js
+++ core.js
@@ -1,9 +1,10 @@
+var i;
 $(window).on("load", function(){
 	$(".img").addClass("fadeIn").css("opacity", "1.0");
 	$("h1, p, ul").addClass("fadeInRight");
 	$("h1").css("animation-delay", "0.5s");
 	$("p").css("animation-delay", "0.25s");
-	var i = setInterval(function(){setOpaque();}, 1000);
+	i = setInterval(function(){setOpaque();}, 1000);
 });
 function setOpaque(){
 	$("h1, p, ul").css("opacity", "1.0")
